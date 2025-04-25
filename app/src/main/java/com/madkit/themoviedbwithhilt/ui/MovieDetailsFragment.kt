f61e0039ac86bca613329d269d5cfacaf9b2aa4f
demirtasm <m.ddemirtas@hotmail.com>
2025-04-25 14:07:21 +0300

    first push

---
+++ app/src/main/java/com/madkit/themoviedbwithhilt/ui/MovieDetailsFragment.kt
@@ -0,0 +1,20 @@
+package com.madkit.themoviedbwithhilt.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.madkit.themoviedbwithhilt.R
+
+class MovieDetailsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_movie_details, container, false)
+    }
+}
